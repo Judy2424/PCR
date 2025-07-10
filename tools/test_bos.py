33e511ed0ee16113d33f46005f6dffb14b5ccfa6
Judy2424 <166384429+Judy2424@users.noreply.github.com>
2025-07-10 18:20:24 +0900

    Update test_bos.py

--- tools/test_bos.py
+++ tools/test_bos.py
@@ -12,6 +12,8 @@ from mmcv.cnn import fuse_conv_bn
 from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
                          wrap_fp16_model)
 
+import sys
+sys.path.append(os.getcwd())
 from mmdet.apis import multi_gpu_test, single_gpu_test
 from mmdet.datasets import (build_dataloader, build_dataset,
                             replace_ImageToTensor)
